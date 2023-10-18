ba725cdbc54750b30e019c78de5eb28e04a59955
Miroslav Nedelchev <miroslavn94@gmail.com>
2023-10-18 13:24:34 +0300

    hardhat setup commit

--- hardhat/README.md
+++ hardhat/README.md
@@ -1,1 +1,14 @@
-# Hardhat Examples
+# Neon tutorials with Hardhat
+
+### Setup terminal commands:
+* ```npm install``` - Downloading required packages.
+
+### Deployment terminal commands:
+* ```npx hardhat run scripts/TestERC20/deploy.js --network neondevnet``` - Sample command of deploying ``contracts/TestERC20/TestERC20.sol`` on Neon Devnet network. If you wish to deploy on Neon Mainnet then change the ``--network`` parameter to ``neonmainnet``.
+* ```npx hardhat verify --network neondevnet <CONTRACT_ADDRESS>``` - Sample command of verifying a contract deployed on Neon Devnet network. ``<CONTRACT_ADDRESS>`` parameter has to be replaced with the smart contract address.
+
+#### Before starting make sure to create .env file containing the following data ( make a copy of .env.example file and rename it to .env ):
+```
+    PRIVATE_KEY_OWNER=XYZ
+```
+- *PRIVATE_KEY_OWNER - the private key used to deploy the contracts.*
---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.21",
+    etherscan: {
+        apiKey: {
+            neonevm: "test"
+        },
+        customChains: [
+            {
+                network: "neonevm",
+                chainId: 245022926,
+                urls: {
+                    apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
+                    browserURL: "https://devnet.neonscan.org"
+                }
+            }
+        ]
+    },
+    networks: {
+        neondevnet: {
+            url: "https://devnet.neonevm.org",
+            accounts: [process.env.PRIVATE_KEY_OWNER],
+            chainId: 245022926,
+            allowUnlimitedContractSize: false,
+            gas: "auto",
+            gasPrice: "auto",
+            isFork: true
+        },
+        neonmainnet: {
+            url: "https://neon-proxy-mainnet.solana.p2p.org",
+            accounts: [process.env.PRIVATE_KEY_OWNER],
+            chainId: 245022934,
+            allowUnlimitedContractSize: false,
+            gas: "auto",
+            gasPrice: "auto",
+            isFork: true
+        }
+    }
+};
