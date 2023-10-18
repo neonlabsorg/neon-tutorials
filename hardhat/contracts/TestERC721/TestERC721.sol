85daf20eb94a9d3398f60e2f8cdccb1e1c51e245
Miroslav Nedelchev <miroslavn94@gmail.com>
2023-10-18 20:14:02 +0300

    add ERC721 example

---
+++ hardhat/contracts/TestERC721/TestERC721.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestERC721 is ERC721, Ownable {
+    constructor() ERC721("TestERC721", "TST") Ownable(msg.sender) {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
