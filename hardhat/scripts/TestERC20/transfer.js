6003b06ea19fd51910c36c5469a5ee3fb05ef26c
Miroslav Nedelchev <miroslavn94@gmail.com>
2023-10-19 19:27:07 +0300

    work on videos

---
+++ hardhat/scripts/TestERC20/transfer.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers, hre } = require("hardhat");
+
+async function main() {
+    const TestERC20Address = ''; // !!! paste here your deployed smart contract address !!!
+    if (!ethers.isAddress(TestERC20Address)) {
+        console.log('Invalid TestERC20Address');
+        return false;
+    }
+
+    const receiver = ethers.Wallet.createRandom(); // !!! change this to valid address on production !!!
+    const TestERC20 = await ethers.getContractAt('TestERC20', TestERC20Address);
+
+    console.log('Receiver balance before transfer', await TestERC20.balanceOf(receiver.address));
+
+    await TestERC20.transfer(receiver, 1000);
+
+    console.log('Receiver balance after transfer', await TestERC20.balanceOf(receiver.address));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
