dc233d6a0cc9faad8e2a30f53e48590baec5a6ea
Miroslav Nedelchev <miroslavn94@gmail.com>
2023-10-27 12:26:38 +0300

    initial commit

---
+++ ERC20ForSPL/hardhat.config.js
@@ -0,0 +1,54 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.21",
+    etherscan: {
+        apiKey: {
+            neonevm: "test"
+        },
+        customChains: [
+            {
+                network: "neonevm",
+                chainId: 245022926,
+                urls: {
+                    apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
+                    browserURL: "https://devnet.neonscan.org"
+                }
+            }
+        ]
+    },
+    networks: {
+        neondevnet: {
+            url: "https://devnet.neonevm.org",
+            accounts: [process.env.USER1_KEY, process.env.PRIVATE_KEY_OWNER, process.env.USER2_KEY, process.env.USER3_KEY],
+            chainId: 245022926,
+            allowUnlimitedContractSize: false,
+            gas: "auto",
+            gasPrice: "auto",
+            isFork: true
+        },
+        neonmainnet: {
+            url: "https://neon-proxy-mainnet.solana.p2p.org",
+            accounts: [process.env.PRIVATE_KEY_OWNER, process.env.USER1_KEY, process.env.USER2_KEY, process.env.USER3_KEY],
+            chainId: 245022934,
+            allowUnlimitedContractSize: false,
+            gas: "auto",
+            gasPrice: "auto",
+            isFork: true
+        },
+        /* hardhat: {
+            forking: {
+                live: false,
+                saveDeployments: false,
+                accounts: [process.env.PRIVATE_KEY_OWNER, process.env.USER1_KEY, process.env.USER2_KEY, process.env.USER3_KEY],
+                url: "https://devnet.neonevm.org"
+            }
+        } */
+    },
+    mocha: {
+        timeout: 180000
+    }
+};
