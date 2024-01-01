8dd783b8bb37b3c045226d47b3fd4ac8cafd0ce7
Miroslav Nedelchev <miroslavn94@gmail.com>
2024-01-01 20:56:40 +0200

    add natspec for ERC20ForSPL and ERC20ForSPLMintable

--- ERC20ForSPL/contracts/test/ERC20ForSPLMintableV2.sol
+++ ERC20ForSPL/contracts/test/ERC20ForSPLMintableV2.sol
@@ -7,7 +7,7 @@ import '../ERC20ForSPLMintable.sol';
 // !!! This contract shouldn't be used in production, it's made only to validate the upgradibility on Neon network !!!
 // !!!!!!!!!!!!!!!!!
 contract ERC20ForSPLMintableV2 is ERC20ForSPL {
-    function getDummyData() public view returns(uint256) {
+    function getDummyData() public pure returns(uint256) {
         return 12345;
     }
 }
--- ERC20ForSPL/contracts/test/ERC20ForSPLV2.sol
+++ ERC20ForSPL/contracts/test/ERC20ForSPLV2.sol
@@ -7,7 +7,7 @@ import '../ERC20ForSPL.sol';
 // !!! This contract shouldn't be used in production, it's made only to validate the upgradibility on Neon network !!!
 // !!!!!!!!!!!!!!!!!
 contract ERC20ForSPLV2 is ERC20ForSPL {
-    function getDummyData() public view returns(uint256) {
+    function getDummyData() public pure returns(uint256) {
         return 12345;
     }
 }
