3a2ad92b09deba1049c23efa5fe124a835780550
Miroslav Nedelchev <miroslavn94@gmail.com>
2024-01-16 00:22:39 +0200

    change to latest stable version of Solidity, tests ran with 0.8.23 and everything passes successfully

--- ERC20ForSPL/contracts/ERC20ForSPL.sol
+++ ERC20ForSPL/contracts/ERC20ForSPL.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity 0.8.23;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
--- ERC20ForSPL/hardhat.config.js
+++ ERC20ForSPL/hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.21",
+    solidity: "0.8.23",
     etherscan: {
         apiKey: {
             neonevm: "test"
