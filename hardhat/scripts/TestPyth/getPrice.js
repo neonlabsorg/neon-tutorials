8d3fc55511530b2364b0d72a97634dd306fae2ed
sukanyaparashar <sukanyaparashar111@gmail.com>
2024-03-25 15:59:37 -0600

    Change the service endpoint

--- hardhat/scripts/TestPyth/getPrice.js
+++ hardhat/scripts/TestPyth/getPrice.js
@@ -5,43 +5,47 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const { ethers, network } = require("hardhat");
-const {EvmPriceServiceConnection} = require("@pythnetwork/pyth-evm-js");
-const { NEON_CONFIG } = require('../NEON_CONFIG');
+const { EvmPriceServiceConnection } = require("@pythnetwork/pyth-evm-js");
+const { NEON_CONFIG } = require("../NEON_CONFIG");
 
 async function main() {
-    const TestPythAddress = ''; // paste here your deployed smart contract address
-    if (!ethers.isAddress(TestPythAddress)) {
-        console.log('Invalid TestPythAddress');
-        return false;
-    }
-    
-    const TestPyth = await ethers.getContractAt('TestPyth', TestPythAddress);
-    const [owner] = await ethers.getSigners();
-    const connection = new EvmPriceServiceConnection(
-        "https://xc-testnet.pyth.network"
-    ); // See Price Service endpoints section below for other endpoints
+  const TestPythAddress = ""; // paste here your deployed smart contract address
+  if (!ethers.isAddress(TestPythAddress)) {
+    console.log("Invalid TestPythAddress");
+    return false;
+  }
 
-    let priceIds;
-    if (network.name == 'neondevnet') {
-        priceIds = NEON_CONFIG.DEVNET.PYTH.PRICE_FEEDS;
-    } else if (network.name == 'neonmainnet') {
-        pythAddress = NEON_CONFIG.MAINNET.PYTH.PRICE_FEEDS;
-    }
-    const priceUpdateData = await connection.getPriceFeedsUpdateData(Object.values(priceIds));
+  const TestPyth = await ethers.getContractAt("TestPyth", TestPythAddress);
+  const [owner] = await ethers.getSigners();
+  const connection = new EvmPriceServiceConnection(
+    "https://hermes.pyth.network"
+  );
 
-    // fee has to be paid when you update Pyth price feeds
-    const fee = await TestPyth.getUpdateFee(priceUpdateData);
-    let tx = await TestPyth.connect(owner).updatePriceFeeds(priceUpdateData, {value: fee});
-    await tx.wait(3);
+  let priceIds;
+  if (network.name == "neondevnet") {
+    priceIds = NEON_CONFIG.DEVNET.PYTH.PRICE_FEEDS;
+  } else if (network.name == "neonmainnet") {
+    pythAddress = NEON_CONFIG.MAINNET.PYTH.PRICE_FEEDS;
+  }
+  const priceUpdateData = await connection.getPriceFeedsUpdateData(
+    Object.values(priceIds)
+  );
 
-    for (const key in priceIds) {
-        console.log(key, await TestPyth.getPrice(priceIds[key]));
-    }
+  // fee has to be paid when you update Pyth price feeds
+  const fee = await TestPyth.getUpdateFee(priceUpdateData);
+  let tx = await TestPyth.connect(owner).updatePriceFeeds(priceUpdateData, {
+    value: fee,
+  });
+  await tx.wait(3);
+
+  for (const key in priceIds) {
+    console.log(key, await TestPyth.getPrice(priceIds[key]));
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
